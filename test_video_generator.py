cdb3d4d3aa4147bc6cba08768c94ab2fd6ef454d
sfauvel <sfauvel@gmail.com>
2024-02-14 22:08:14 +0100

    Move tests in a separate file

---
+++ test_video_generator.py
@@ -0,0 +1,135 @@
+
+
+from datetime import timedelta, datetime
+from enum import Enum
+import glob
+import os
+import re
+import shutil
+import sys
+import unittest
+
+import moviepy.editor as mpy
+from moviepy.video import fx
+from moviepy.video.tools.segmenting import findObjects
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+from video_generator import *
+
+class TestVideoGenerator(unittest.TestCase):
+
+    def test_event_record_to_csv(self):
+        assert "3;A;12;2" == EventRecord(3, "A", 12, 2).to_csv()
+        
+    def test_event_record_to_csv_without_quarter(self):
+        assert "3;A;12" == EventRecord(3, "A", 12, None).to_csv()
+
+
+    def test_event_record_from_csv(self):
+        record = EventRecord.from_csv("3;A;12;2")
+        assert 3 == record.points
+        assert "A" == record.team
+        assert 12 == record.time_in_seconds
+        assert 2 == record.quarter_time
+
+    def test_event_record_from_csv_without_quarter(self):
+        record = EventRecord.from_csv("3;A;12")
+        assert None == record.quarter_time
+        
+    def test_event_record_from_csv_with_time_in_minutes(self):
+        record = EventRecord.from_csv("3;A;4:25;2")
+        assert 4*60+25 == record.time_in_seconds
+
+    def test_generate_score(self):
+        print(generate_score([
+                (2, EQUIPE.A, 3),
+                (2, EQUIPE.B, 5),
+                (1, EQUIPE.A, 8),
+            ]))
+        
+        self.assertEqual([
+                (0, 0, 0),
+                (2, 0, 3),
+                (2, 2, 5),
+                (3, 2, 8),
+            ], 
+            generate_score([
+                (2, EQUIPE.A, 3),
+                (2, EQUIPE.B, 5),
+                (1, EQUIPE.A, 8),
+            ])
+        )
+    
+    def test_extract_infos(self):
+        infos = EventFile().extract_lines_infos([
+            "2;A;3",
+            "1;B;5",
+            "3;A;7",
+            "0;A;10",
+        ], 0, 0, "A", "B", 0)
+        assert infos[0] == ("A 0 - 0 B", 0, 0, 0, 3)
+        assert infos[1] == ("A 2 - 0 B", 2, 0, 3, 5)
+        assert infos[2] == ("A 2 - 1 B", 2, 1, 5, 7)
+        assert infos[3] == ("A 5 - 1 B", 5, 1, 7, 10)
+        assert len(infos) == 4
+        
+    def test_extract_infos_with_several_formats(self):
+        infos = EventFile().extract_lines_infos([
+            "2;A;3s",
+            "1;B;5",
+            "3;A;3:12",
+            "0;A;1:02:14",
+        ], 0, 0, "A", "B", 0)
+        assert infos[0] == ("A 0 - 0 B", 0, 0, 0, 3)
+        assert infos[1] == ("A 2 - 0 B", 2, 0, 3, 5)
+        assert infos[2] == ("A 2 - 1 B", 2, 1, 5, 192)
+        assert infos[3] == ("A 5 - 1 B", 5, 1, 192, 3734)
+        assert len(infos) == 4
+        
+    def test_extract_infos_with_several_formats_with_an_initial_score_and_time(self):
+        infos = EventFile().extract_lines_infos([
+            "2;A;3s",
+            "0;A;8s",
+        ], 5, 8, "A", "B", 10)
+        
+        assert infos[0] == ("A 5 - 8 B", 5, 8, 10, 13), infos[0]
+        assert infos[1] == ("A 7 - 8 B", 7, 8, 13, 18), infos[1]
+
+    def test_time_to_seconds(self):
+        assert 5 == time_to_seconds("5")
+        assert 5 == time_to_seconds("5s")
+        assert 4*60+25 == time_to_seconds("4:25")
+        assert 2*3600+14*60+25 == time_to_seconds("2:14:25")
+
+    def test_display_score(self):
+        shutil.rmtree("tmp")
+        os.makedirs("tmp", exist_ok=True)
+        events = [EventRecord(2,"A",5),EventRecord(1,"B",6),EventRecord(3,"A",7),EventRecord(0,"X",9)]
+        with (open("tmp/tmp1.csv", "w")) as csv_file:
+            csv_file.write("\n".join([e.to_csv() for e in events]))
+            
+        match = MatchVideo("", "A", "B")
+        match.csv_folder = "tmp"
+        output = match.display_score()
+        assert output == "0: A 0 - 0 B\n5: A 2 - 0 B\n6: A 2 - 1 B\n7: A 5 - 1 B"
+        
+    def test_display_score_from_several_files(self):
+        shutil.rmtree("tmp")
+        os.makedirs("tmp", exist_ok=True)
+        events = [EventRecord(2,"A",5),EventRecord(1,"B",6),EventRecord(3,"A",7),EventRecord(0,"X",9)]
+        with (open("tmp/tmp1.csv", "w")) as csv_file:
+            csv_file.write("\n".join([e.to_csv() for e in events]))
+            
+        events = [EventRecord(3,"B",2),EventRecord(2,"A",4),EventRecord(0,"X",10)]
+        with (open("tmp/tmp2.csv", "w")) as csv_file:
+            csv_file.write("\n".join([e.to_csv() for e in events]))
+            
+        match = MatchVideo("", "A", "B")
+        match.csv_folder = "tmp"
+        output = match.display_score()
+        assert output == "0: A 0 - 0 B\n5: A 2 - 0 B\n6: A 2 - 1 B\n7: A 5 - 1 B\n11: A 5 - 4 B\n13: A 7 - 4 B", output
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
--- video_generator.py
+++ video_generator.py
@@ -38,117 +38,6 @@ def generate_score(points):
         score_evolution.append((a,b,time))
     return score_evolution
 
-class TestVideoGenerator(unittest.TestCase):
-
-    def test_event_record_to_csv(self):
-        assert "3;A;12;2" == EventRecord(3, "A", 12, 2).to_csv()
-        
-    def test_event_record_to_csv_without_quarter(self):
-        assert "3;A;12" == EventRecord(3, "A", 12, None).to_csv()
-
-
-    def test_event_record_from_csv(self):
-        record = EventRecord.from_csv("3;A;12;2")
-        assert 3 == record.points
-        assert "A" == record.team
-        assert 12 == record.time_in_seconds
-        assert 2 == record.quarter_time
-
-    def test_event_record_from_csv_without_quarter(self):
-        record = EventRecord.from_csv("3;A;12")
-        assert None == record.quarter_time
-        
-    def test_event_record_from_csv_with_time_in_minutes(self):
-        record = EventRecord.from_csv("3;A;4:25;2")
-        assert 4*60+25 == record.time_in_seconds
-
-    def test_generate_score(self):
-        self.assertEqual([
-                (0, 0, 0),
-                (2, 0, 3),
-                (2, 2, 5),
-                (3, 2, 8),
-            ], 
-            generate_score([
-                (2, EQUIPE.A, 3),
-                (2, EQUIPE.B, 5),
-                (1, EQUIPE.A, 8),
-            ])
-        )
-    
-    def test_extract_infos(self):
-        infos = EventFile().extract_lines_infos([
-            "2;A;3",
-            "1;B;5",
-            "3;A;7",
-            "0;A;10",
-        ], 0, 0, "A", "B", 0)
-        assert infos[0] == ("A 0 - 0 B", 0, 0, 0, 3)
-        assert infos[1] == ("A 2 - 0 B", 2, 0, 3, 5)
-        assert infos[2] == ("A 2 - 1 B", 2, 1, 5, 7)
-        assert infos[3] == ("A 5 - 1 B", 5, 1, 7, 10)
-        assert len(infos) == 4
-        
-    def test_extract_infos_with_several_formats(self):
-        infos = EventFile().extract_lines_infos([
-            "2;A;3s",
-            "1;B;5",
-            "3;A;3:12",
-            "0;A;1:02:14",
-        ], 0, 0, "A", "B", 0)
-        assert infos[0] == ("A 0 - 0 B", 0, 0, 0, 3)
-        assert infos[1] == ("A 2 - 0 B", 2, 0, 3, 5)
-        assert infos[2] == ("A 2 - 1 B", 2, 1, 5, 192)
-        assert infos[3] == ("A 5 - 1 B", 5, 1, 192, 3734)
-        assert len(infos) == 4
-        
-    def test_extract_infos_with_several_formats_with_an_initial_score_and_time(self):
-        infos = EventFile().extract_lines_infos([
-            "2;A;3s",
-            "0;A;8s",
-        ], 5, 8, "A", "B", 10)
-        
-        assert infos[0] == ("A 5 - 8 B", 5, 8, 10, 13), infos[0]
-        assert infos[1] == ("A 7 - 8 B", 7, 8, 13, 18), infos[1]
-
-    def test_time_to_seconds(self):
-        assert 5 == time_to_seconds("5")
-        assert 5 == time_to_seconds("5s")
-        assert 4*60+25 == time_to_seconds("4:25")
-        assert 2*3600+14*60+25 == time_to_seconds("2:14:25")
-
-    def test_display_score(self):
-        shutil.rmtree("tmp")
-        os.makedirs("tmp", exist_ok=True)
-        events = [EventRecord(2,"A",5),EventRecord(1,"B",6),EventRecord(3,"A",7),EventRecord(0,"X",9)]
-        with (open("tmp/tmp1.csv", "w")) as csv_file:
-            csv_file.write("\n".join([e.to_csv() for e in events]))
-            
-        match = MatchVideo("", "A", "B")
-        match.csv_folder = "tmp"
-        output = match.display_score()
-        assert output == "0: A 0 - 0 B\n5: A 2 - 0 B\n6: A 2 - 1 B\n7: A 5 - 1 B"
-        
-    def test_display_score_from_several_files(self):
-        shutil.rmtree("tmp")
-        os.makedirs("tmp", exist_ok=True)
-        events = [EventRecord(2,"A",5),EventRecord(1,"B",6),EventRecord(3,"A",7),EventRecord(0,"X",9)]
-        with (open("tmp/tmp1.csv", "w")) as csv_file:
-            csv_file.write("\n".join([e.to_csv() for e in events]))
-            
-        events = [EventRecord(3,"B",2),EventRecord(2,"A",4),EventRecord(0,"X",10)]
-        with (open("tmp/tmp2.csv", "w")) as csv_file:
-            csv_file.write("\n".join([e.to_csv() for e in events]))
-            
-        match = MatchVideo("", "A", "B")
-        match.csv_folder = "tmp"
-        output = match.display_score()
-        assert output == "0: A 0 - 0 B\n5: A 2 - 0 B\n6: A 2 - 1 B\n7: A 5 - 1 B\n11: A 5 - 4 B\n13: A 7 - 4 B", output
-        
-        
-
-###################################################################
-
 def generate_test_video():
 
     sb_logo = mpy.ImageClip(SB_LOGO_PATH).\
@@ -643,7 +532,7 @@ if __name__ == "__main__":
         # Compress with direct write_videofile fps=24, medium: 473.6.8mo
         #audio_analyze("Match_2024_02_04/output/VID_20240204_110324.output.mp4")
     elif args[1] == "record":
-        Recorder().record_input("output.csv")    
+        Recorder().record_input("tmp/output.csv")    
     elif args[1] == "generate":
         #spike("VID_20240204_105730")
         generate_from_dir("short", "short", "short", team_a="SLB", team_b="VISITOR")
